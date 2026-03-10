43512164b6f7470ac98e52d41a152b2b1a8f8bdd
The-Peppermint-Snake <brkovalik@gmail.com>
2026-03-10 13:38:00 -0400

    working on js now

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //karma system? to low would trigger things
+const img = document.getElementById("img");
+const caption = document.getElementById("cap");
+const opt1 = document.getElementById("opt1");
+const opt2 = document.getElementById("opt2");
+const cont = document.getElementById("cont");
+
+let outside_tent = {
+    img: "",
+    cap: ""
+}
+
+let storyNodes = [outside_tent]
+
+cont.addEventListener("click", displayScene());
+
+function displayScene(i) {
+    let node = storyNodes[i];
+    img.src = node.img;
+    caption.textContent = node.cap
+}
