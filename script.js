3bd5e78fd4675b1cce65791f41b4e15354f8ffaf
The-Peppermint-Snake <brkovalik@gmail.com>
2026-03-10 14:08:14 -0400

    uuhhhhh im screwed

--- script.js
+++ script.js
@@ -1,21 +1,22 @@
 //karma system? to low would trigger things
 const img = document.getElementById("img");
-const caption = document.getElementById("cap");
+const cap = document.getElementById("cap");
 const opt1 = document.getElementById("opt1");
 const opt2 = document.getElementById("opt2");
 const cont = document.getElementById("cont");
 
 let outside_tent = {
-    img: "",
-    cap: ""
+    image: "images/campfire_embers.jpg",
+    caption: "You finish making your breakfast. You tried to put out the fire but it is still warm. Do you put more water on it?"
+    //hid cont and unhide opt1 and opt2
 }
 
 let storyNodes = [outside_tent]
 
-cont.addEventListener("click", displayScene());
+cont.addEventListener("onclick", displayScene(0));
 
 function displayScene(i) {
     let node = storyNodes[i];
-    img.src = node.img;
-    caption.textContent = node.cap
+    img.src = node.image;
+    cap.textContent = node.caption
 }
